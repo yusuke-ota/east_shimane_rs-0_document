d9b620cfe65f16d7dd2305ccf994ef3adabec2b0
yusuke-ota <33338085+yusuke-ota@users.noreply.github.com>
2020-01-19 19:43:54 +0900

    コミット漏れ

---
+++ value/practice.rs
@@ -0,0 +1,45 @@
+fn practice() {
+    // let, mut, {}を追加して、Runが通るようにしてください
+    let value = "変数1";
+    assert_eq!(value, "変数1");
+
+    value = 1.0;
+    assert_eq!(value, 1.0);
+
+    value = 2.0;
+    assert_eq!(value, 2.0);
+
+    value = "変数2";
+    assert_eq!(value, "変数2");
+}
+
+// 解法1
+
+// let value = "変数1";
+// assert_eq!(value, "変数1");
+
+// {
+//     let mut value = 1.0;
+//     assert_eq!(value, 1.0);
+
+//     value = 2.0;
+//     assert_eq!(value, 2.0);
+// }
+
+// value = "変数2";
+// assert_eq!(value, "変数2");
+
+
+// 解法2
+
+// let value = "変数1";
+// assert_eq!(value, "変数1");
+
+// let mut value = 1.0;
+// assert_eq!(value, 1.0);
+
+// value = 2.0;
+// assert_eq!(value, 2.0);
+
+// let value = "変数2";
+// assert_eq!(value, "変数2");
