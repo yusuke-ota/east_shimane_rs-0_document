4c0ab1d04f1da8bb0ec51db64be9f405f216b4b6
yusuke ota <33338085+yusuke-ota@users.noreply.github.com>
2020-03-05 12:54:21 +0900

    add shadowing_message

--- examples/shadowing_example.rs
+++ examples/shadowing_example.rs
@@ -1,5 +1,46 @@
-use workshop::value::shadowing_pointer;
-
 fn main() {
-    shadowing_pointer::explain();
+    println!("--- Step1 ---");
+    // 変数新規作成
+    // | id | value |
+    // |:--:| :--:  |
+    // |  1 |"変数1"|
+    let value = "変数1";
+    println!("{}", value);
+
+    println!("--- Step2 ---");
+    // シャード―イング
+    // | id | value |
+    // |:--:| :--:  |
+    // |  2 |  1.0  |
+    // |  1 |"変数1"|
+    let value = 1.0;
+    println!("{}", value);
+
+    {
+        // シャード―イング
+        // | id | value |
+        // |:--:| :--:  |
+        // |  3 |  2.0  |
+        // |  2 |  1.0  |
+        // |  1 |"変数1"|
+        println!("--- Step3 ---");
+        let value = 2.0;
+        println!("{}", value);
+    } // <- スコープを抜ける
+    // | id | value |
+    // |:--:| :--:  |
+    // |  2 |  1.0  |
+    // |  1 |"変数1"|
+    println!("--- Step4 ---");
+    println!("{}", value);
+
+    // シャード―イング
+    // | id | value |
+    // |:--:| :--:  |
+    // |  3 |"変数2"|
+    // |  2 |  1.0  |
+    // |  1 |"変数1"|
+    println!("--- Step5 ---");
+    let value = "変数2";
+    println!("{}", value);
 }
